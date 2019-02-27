19a20220b539172846c86ade3004602aef3afdd4
Samuel Dias <42343502+Nikenilson@users.noreply.github.com>
2019-02-27 11:50:04 -0300

    comitado

--- javascript/xadrez.js
+++ javascript/xadrez.js
@@ -1,17 +1,25 @@
+alert("lixuuu")
 var objCanvas=null; // objeto que representa o canvas
 var objContexto=null; // objeto que representa o contexto do canvas
 
 var imgFundo = new Image();
-imgFundo.src = "fundo.png";
+imgFundo.src = "../images/fundo.jpg";
 
 objCanvas = document.getElementById("meuCanvas");
 objContexto = objCanvas.getContext("2d");
 objContexto.drawImage(imgFundo,0,0);
 
-function Iniciar()
+window.onload = function()
 {
 	objCanvas = document.getElementById("meuCanvas");
 	objContexto = objCanvas.getContext("2d");
 
-	//AtualizaTela();
+	AtualizaTela();
 }
+
+function AtualizaTela()
+{
+	objContexto.drawImage(imgFundo,0,0);
+
+}
+
