d6fa0078df684881544198e24e92de0d49c02662
KaiFlame <42344215+KaiFlame@users.noreply.github.com>
2019-04-16 11:45:25 -0300

    dfgdfg

---
+++ Arce-Chess/Arce-Chess/Content/regras.bat
@@ -0,0 +1,4 @@
+@echo off
+sass --watch regras.scss:regras.css
+sleep 10
+exit
---
+++ Arce-Chess/Arce-Chess/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Arce_Chess.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Regras()
+        {
+            return View();
+        }
+
+        public ActionResult SobreNos()
+        {
+            return View();
+        }
+
+        public ActionResult Perfil()
+        {
+            return View();
+        }
+    }
+}
