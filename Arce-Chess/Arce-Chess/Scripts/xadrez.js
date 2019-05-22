4a35927f9945b33edcfc2c5e4cfd4a3bdb695510
Samuel Dias <42343502+Nikenilson@users.noreply.github.com>
2019-05-22 11:25:14 -0300

    De papo em papo, a galinha enche o grão

--- Arce-Chess/Arce-Chess/Scripts/xadrez.js
+++ Arce-Chess/Arce-Chess/Scripts/xadrez.js
@@ -1,11 +1,9 @@
-//Tem que mudar ainda
-
 function Xadrez(tamanho, pieces)
 {
     this.tamanho = tamanho;
+    this.setPiece(pieces);
     
-    this.pecaSelecionada = null;
-    
+    this.pieceSelecionada = null;
     this.vez = "brancas";
     
     this.canvas        = document.getElementById("game");
@@ -13,192 +11,200 @@ function Xadrez(tamanho, pieces)
     
     this.canvas.width  = (this.tamanho==8)?720:900;
     this.canvas.height = 720;
+
+    var elem = document.getElementById('game'),
+        elemLeft = elem.offsetLeft,
+        elemTop = elem.offsetTop,
+        context = elem.getContext('2d');
     
     this.setTabuleiro();
-    this.setPeca(pieces);
-    
-    var elem = document.getElementById('game'),
-    elemLeft = elem.offsetLeft,
-    elemTop  = elem.offsetTop,
-    context  = elem.getContext('2d');
-    
+
+
     pieces = this.pieces;
-    
     this.matriz = null;
-    
     var funcAux = this;
     
     //onClick do canvas
-    this.canvas.addEventListener('click', function(event) {
-    var x = event.pageX - elemLeft,
-        y = event.pageY - elemTop;
-        
-        var pecaSelecionada = null;
-        
-        pieces.forEach(function(piece)
+    this.canvas.
+        addEventListener('click',function (event)
         {
-            if(piece.getTX(tamanho) < x && piece.getTX(tamanho) + 80 > x)
-                if(piece.getTY(tamanho) < y && piece.getTY(tamanho) + 80 > y)
-                    pecaSelecionada = piece;           
-        });
+            var x = event.pageX - elemLeft,
+                y = event.pageY - elemTop;
+            var pieceSelecionada = null;
         
-        funcAux.montarMatriz();
+            pieces.forEach(function(piece)
+            {
+                if(piece.getTX(tamanho) < x && piece.getTX(tamanho) + 80 > x)
+                    if(piece.getTY(tamanho) < y && piece.getTY(tamanho) + 80 > y)
+                        pieceSelecionada = piece;           
+            });
         
-        if (pecaSelecionada != null)
-        {
-            if (pecaSelecionada.cor != funcAux.vez)
+            funcAux.montarMatriz();
+        
+            if (pieceSelecionada != null)
             {
-                if (funcAux.pecaSelecionada != null)
-                    {
-                        funcAux.pecaSelecionada.paraOndePodeIr(funcAux.matriz, funcAux.tamanho).forEach(
-                        function(coordenada)
-                        {
-                            if (coordenada.x == Math.floor(x / ((funcAux.tamanho == 8) ? 90 : 80)) && coordenada.y == Math.floor(y/((funcAux.tamanho == 8)?90:80)))
-                                {
-                                    funcAux.comer(x, y);
-                                }
-                        });
-                    }
-                
-                return;
-            }                    
-            
-            if (pecaSelecionada == funcAux.pecaSelecionada)
-            {
-                funcAux.updateScreen();
-                funcAux.pecaSelecionada = null;
-            }
-            else
+                if (pieceSelecionada.cor != funcAux.vez)
                 {
-                    funcAux.putGreenBalls(selectedPiece.whereCanGo(funcAux.matriz, funcAux.tamanho) , selectedPiece);
-                    funcAux.selectedPiece = selectedPiece;
+                    if (funcAux.pieceSelecionada != null)
+                        {
+                        funcAux.pieceSelecionada.paraOndePodeIr(funcAux.matriz, funcAux.tamanho).forEach(
+                            function(coordenada)
+                            {
+                                if (coordenada.x == Math.floor(x / ((funcAux.tamanho == 8) ? 90 : 80)) && coordenada.y == Math.floor(y/((funcAux.tamanho == 8)?90:80)))
+                                    {
+                                        funcAux.comer(x, y);
+                                    }
+                            });
+                        }
+                
+                    return;
+                }                    
+            
+                if (pieceSelecionada == funcAux.pieceSelecionada)
+                {
+                    funcAux.atualizarTela();
+                    funcAux.pieceSelecionada = null;
+                }
+                else
+                {
+                    funcAux.putGreenBalls(pieceSelecionada.paraOndePodeAndar(funcAux.matriz, funcAux.tamanho), pieceSelecionada);
+                    funcAux.pieceSelecionada = pieceSelecionada;
                 }
             
-        }
-        else
-            if(funcAux.selectedPiece != null)
+            }
+            else
+                if (funcAux.pieceSelecionada != null)
                 {
-                    funcAux.selectedPiece.whereCanGo(funcAux.matriz, funcAux.tamanho).forEach(
-                    function(coordinate)
+                    funcAux.pieceSelecionada.paraOndePodeAndar(funcAux.matriz, funcAux.tamanho).forEach(
+                    function(coordenada)
                     {
-                        if(coordinate.x == Math.floor(x/((funcAux.tamanho == 8)?90:80)) && coordinate.y == Math.floor(y/((funcAux.tamanho == 8)?90:80)))
-                            {
-                                funcAux.walk(x, y);
-                            }
+                        if (coordenada.x == Math.floor(x / ((funcAux.tamanho == 8) ? 90 : 80)) && coordenada.y == Math.floor(y / ((funcAux.tamanho == 8) ? 90 : 80)))
+                        {
+                            funcAux.andar(x, y);
+                        }
                               
                     });
                 }
-    }, false);
+        }, false);
+    //Fim do onClick do canvas
     
-    this.updateScreen();
+    this.atualizarTela();
 }
 
-Chess.prototype.andar = function(x, y)
-{
-    var sPX    = this.selectedPiece.getX();
-    var sPY    = this.selectedPiece.getY();
-    this.selectedPiece.setXY(Math.floor(x/((this.tamanho == 8)?90:80)), Math.floor(y/((this.tamanho == 8)?90:80)));
-    
-    if(!this.isCheck((this.turn == "white")?("black"):("white")))
-    {
-        this.selectedPiece = null;
-        if(this.isCheck(this.turn))
-            alert("Check");
-        this.turn  = (this.turn == "white")?("black"):("white");
-        this.updateScreen();
-        return true;
-    }
-    else
-        {
-            this.selectedPiece.setXY(sPX,sPY);
-            return false;
+Chess.prototype.comer = function (x, y) {
+    var sPX = this.pieceSelecionada.getX();
+    var sPY = this.pieceSelecionada.getY();
+    var aux;
+    for (var i = 0; i < this.pieces.length; i++)
+        if (this.pieces[i].getX() == Math.floor(x / ((this.tamanho == 8) ? 90 : 80)) && this.pieces[i].getY() == Math.floor(y / ((this.tamanho == 8) ? 90 : 80))) {
+            aux = this.pieces[i];
+            this.pieces.splice(i, 1);
         }
-}
+    this.pieceSelecionada.setXY(Math.floor(x / ((this.tamanho == 8) ? 90 : 80)), Math.floor(y / ((this.tamanho == 8) ? 90 : 80)));
 
-Chess.prototype.comer = function(x, y)
-{
-    var sPX    = this.selectedPiece.getX();
-    var sPY    = this.selectedPiece.getY();
-    var save;
-    for(var i = 0; i < this.pieces.length; i++)
-            if(this.pieces[i].getX() == Math.floor(x/((this.tamanho == 8)?90:80)) && this.pieces[i].getY() == Math.floor(y/((this.tamanho == 8)?90:80)))
-            {
-                save = this.pieces[i];
-                this.pieces.splice(i, 1);
-            }
-    this.selectedPiece.setXY(Math.floor(x/((this.tamanho == 8)?90:80)), Math.floor(y/((this.tamanho == 8)?90:80)));
-                
-    if(!this.isCheck((this.turn == "white")?("black"):("white")))
-    {
-        this.selectedPiece = null;
-        if(this.isCheck(this.turn))
+    if (!this.isCheck((this.vez == "brancas") ? ("pretas") : ("brancas"))) {
+        this.pieceSelecionada = null;
+        if (this.isCheck(this.turn))
             alert("Check");
-        this.turn = (this.turn == "white")?"black":"white";
-        this.updateScreen();
+        this.vez = (this.vez == "brancas") ? "pretas" : "brancas";
+        this.atualizarTela();
         return true;
     }
-    else
-    {
-        this.selectedPiece.setXY(sPX, sPY);
-        this.pieces.push(save);
+    else {
+        this.pieceSelecionada.setXY(sPX, sPY);
+        this.pieces.push(aux);
         return false;
     }
 }
 
-Chess.prototype.setBoard = function()
+Chess.prototype.andar = function(x, y)
+{
+    var sPX = this.pieceSelecionada.getX();
+    var sPY = this.pieceSelecionada.getY();
+    this.pieceSelecionada.setXY(Math.floor(x/((this.tamanho == 8)?90:80)), Math.floor(y/((this.tamanho == 8)?90:80)));
+
+    if (!this.isCheck((this.vez == "brancas") ? ("pretas") : ("brancas")))
+    {
+        this.pieceSelecionada = null;
+        if(this.isCheck(this.turn))
+            alert("Check");
+        this.vez = (this.vez == "brancas") ? ("pretas") : ("brancas");
+        this.atualizarTela();
+        return true;
+    }
+    else
+        {
+            this.pieceSelecionada.setXY(sPX,sPY);
+            return false;
+        }
+}
+
+Chess.prototype.montarMatriz = function () {
+    var matriz = [];
+    for (var i = 0; i < this.tamanho; i++)
+        matriz.push([]);
+    for (var i = 0; i < this.tamanho; i++)
+        for (var j = 0; j < this.tamanho; j++)
+            matriz[i].push();
+
+    this.pieces.forEach(
+        function (piece) {
+            matriz[piece.getX()][piece.getY()] = piece;
+        }
+    );
+
+    this.matriz = matriz;
+}
+
+Chess.prototype.setTabuleiro = function()
 {
     var x = (this.tamanho==8)?90:80;
     
     for(var i = 0; i < 720; i += x)
         for(var j = (i/x)%2==0?x:0; j < 720; j += 2 * x)
-            this.cvsContext.fillRect(j, i, x, x)
+            this.canvasContext.fillRect(j, i, x, x)
     
     if (this.tamanho == 9)
-        this.cvsContext.fillRect(720, 0, 180, 720);
+        this.canvasContext.fillRect(720, 0, 180, 720);
 }
-
-
 
 Chess.prototype.setPieces = function(pieces)
 {
     this.pieces = pieces;
 }
 
-Chess.prototype.updateScreen = function()
+Chess.prototype.atualizarTela = function ()
 {
-     this.cvsContext.clearRect(0, 0, 900, 720);
+    this.canvasContext.clearRect(0, 0, 900, 720);
 
-     this.setBoard();
+     this.setTabuleiro();
     
      for (var i = 0; i < this.pieces.length; i++)
-             this.pieces[i].selfDraw(this.cvsContext, this.tamanho);
+         this.pieces[i].selfDraw(this.canvasContext, this.tamanho);
     
-    if(this.selectedPiece != null)
-        this.putGreenBalls(this.selectedPiece.whereCanGo(this.matriz, this.tamanho));
+     if (this.pieceSelecionada != null)
+         this.putGreenBalls(this.pieceSelecionada.paraOndePodeIr(this.matriz, this.tamanho));
 }
 
-Chess.prototype.putGreenBalls = function(positions, selectedPiece)
+Chess.prototype.putGreenBalls = function (positions, pieceSelecionada)
 {
     /*for(var i = 0; i < this.pieces[i].length; i++)
-        if(this.selectedPiece)*/
+        if(this.pieceSelecionada)*/
     
     var img = new Image();
-    img.src = "img/greenBall.png";
+    img.src = ""; //imagem
     for(var i = 0; i < positions.length; i++)
-    {
-        
-        this.cvsContext.drawImage(img, (this.tamanho==8)?(90 * positions[i].x + 32):(80 * positions[i].x + 27), (this.tamanho==8)?(90 * positions[i].y + 32):(80 * positions[i].y + 27)); 
-        
-    }
+        this.canvasContext.drawImage(img, (this.tamanho==8)?(90 * positions[i].x + 32):(80 * positions[i].x + 27), (this.tamanho==8)?(90 * positions[i].y + 32):(80 * positions[i].y + 27));    
+    
 }
 
-Chess.prototype.isCheck = function(color)
+Chess.prototype.isCheck = function(cor)
 {
     this.montarMatriz();
     var xRei;
     var yRei;
     for(var i = 0; i < this.pieces.length; i++)
-        if (this.pieces[i].value == 0 && this.pieces[i].color != color)
+        if (this.pieces[i].value == 0 && this.pieces[i].cor != cor)
         {
             xRei = this.pieces[i].getX();
             yRei = this.pieces[i].getY();
@@ -206,9 +212,9 @@ Chess.prototype.isCheck = function(color)
     
     
     for(var i = 0; i < this.pieces.length; i++)
-        if(this.pieces[i].color == color)
+        if(this.pieces[i].cor == cor)
         {
-            var aux = this.pieces[i].whereCanGo(this.matriz, this.tamanho);
+            var aux = this.pieces[i].paraOndePodeAndar(this.matriz, this.tamanho);
             for(var j = 0; j < aux.length; j++)
                 if(aux[j].x == xRei && aux[j].y == yRei)
                     return true;
@@ -216,21 +222,3 @@ Chess.prototype.isCheck = function(color)
     return false;
 }
 
-Chess.prototype.montarMatriz = function()
-{
-    var matriz = [];
-        for (var i = 0; i < this.tamanho; i++)
-            matriz.push([]);
-        for (var i = 0; i < this.tamanho; i++)
-            for (var j = 0; j < this.tamanho; j++)
-                matriz[i].push();
-        
-        this.pieces.forEach(
-            function(piece)
-            {
-                matriz[piece.getX()][piece.getY()] = piece;
-            }
-        );
-        
-        this.matriz = matriz;
-}
