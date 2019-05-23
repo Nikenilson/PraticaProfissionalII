d5ddc73fa92b0906d1079aa6db4c97b5980a56aa
KaiFlame <42344215+KaiFlame@users.noreply.github.com>
2019-05-23 00:39:28 -0300

    w

--- Arce-Chess/Arce-Chess/Scripts/Pecas/Pawn.js
+++ Arce-Chess/Arce-Chess/Scripts/Pecas/Pawn.js
@@ -1,9 +1,9 @@
-function Pawn(x,y, color)
+function Pawn(x, y, cor) //Peao fica estranho
 {
     Piece.call(this);
     
-    this.setImages("img/WPawn.png", "img/BPawn.png");
-    this.color = color || "black";
+    this.setImages("", "");
+    this.cor = cor;
     
     this.naosei = false;
     
@@ -12,35 +12,35 @@ function Pawn(x,y, color)
     
     this.naosei = true;
     
-    this.value = 1;
+    this.valor = 1;
     
-    this.hasAlreadymove = false;
+    this.jaMoveu = false;
     
-    this.nomePeca = "pawn";
+    this.nomePiece = "pawn"; //Peao fica estranho
 }
 
 Pawn.prototype = Object.create(Piece.prototype);
 
-Pawn.prototype.whereCanGo = function(m)
+Pawn.prototype.paraOndePodeAndar = function(m)
 {
     var ret = [];
     
-        if(m[this.getX()][this.getY() + ((this.color=="white")?(-1):1)] == null)
-                ret.push(new Coordinate(this.getX(), this.getY() + ((this.color=="white")?(-1):1)));
+    if (m[this.getX()][this.getY() + ((this.cor == "brancas") ? (-1) : 1)] == null)
+        ret.push(new Coordenada(this.getX(), this.getY() + ((this.cor == "brancas") ? (-1) : 1)));
 
-        if (this.hasAlreadymove == false && ret.length > 0)
-            if(m[this.getX()][this.getY() + ((this.color=="white")?(-2):2)] == null)
-                ret.push(new Coordinate(this.getX(), this.getY() + ((this.color=="white")?(-2):2)));
+    if (this.jaMoveu == false && ret.length > 0)
+        if(m[this.getX()][this.getY() + ((this.cor=="brancas")?(-2):2)] == null)
+            ret.push(new Coordenada(this.getX(), this.getY() + ((this.cor=="brancas")?(-2):2)));
 
-        if(this.getX() < m.length - 1)
-            if(m[this.getX() + 1][this.getY() + ((this.color=="white")?(-1):1)] != null)
-                if (m[this.getX() + 1][this.getY() + ((this.color=="white")?(-1):1)].color != this.color)
-                    ret.push(new Coordinate(this.getX() + 1, this.getY() + ((this.color=="white")?(-1):1)));
+    if(this.getX() < m.length - 1)
+        if(m[this.getX() + 1][this.getY() + ((this.cor=="brancas")?(-1):1)] != null)
+            if (m[this.getX() + 1][this.getY() + ((this.cor=="brancas")?(-1):1)].cor != this.cor)
+                ret.push(new Coordenada(this.getX() + 1, this.getY() + ((this.cor=="brancas")?(-1):1)));
 
-        if(this.getX() > 0)
-            if(m[this.getX() - 1][this.getY() + ((this.color=="white")?(-1):1)] != null)
-                if (m[this.getX() - 1][this.getY() + ((this.color=="white")?(-1):1)].color != this.color)
-                    ret.push(new Coordinate(this.getX() - 1, this.getY() + ((this.color=="white")?(-1):1)));
+    if(this.getX() > 0)
+        if(m[this.getX() - 1][this.getY() + ((this.cor=="brancas")?(-1):1)] != null)
+            if (m[this.getX() - 1][this.getY() + ((this.cor == "brancas") ? (-1) : 1)].cor != this.cor)
+                ret.push(new Coordenada(this.getX() - 1, this.getY() + ((this.cor == "brancas") ? (-1) : 1)));
     
     return ret;
 }
@@ -49,7 +49,7 @@ Pawn.prototype.setX = function(x)
 {
     this.x = x;
     
-    this.hasAlreadymove = true;
+    this.jaMoveu = true;
 }
 
 Pawn.prototype.setY = function(y)
@@ -57,14 +57,14 @@ Pawn.prototype.setY = function(y)
     this.y = y;
     
     if(this.naosei == true)
-        if((this.color == "white")?(y == 0):(y == 7))
-            this.promote();
+        if((this.cor == "brancas")?(y == 0):(y == 7))
+            this.promover();
             
-    
-    this.hasAlreadymove = true;
+
+    this.jaMoveu = true;
 }
 
-Pawn.prototype.promote = function()
+Pawn.prototype.promover = function()
 {
     pawn = this;
     $('#modalPromote').modal('show');
